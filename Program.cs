df7424f010c6705d6b48ba4e33dbff82fc8da489
chriswfoster@gmail.com <spinsci@SpinScis-MacBook-Pro-3.local>
2018-07-26 15:25:51 -0500

    PetStore complete

--- Program.cs
+++ Program.cs
@@ -7,10 +7,13 @@ namespace war
         static void Main(string[] args)
         {
             Console.WriteLine("Type your name here:");
-            string yay = Console.ReadLine();
-            Console.WriteLine("You typed: {0}", yay);
+            string userName = Console.ReadLine();
+            Console.WriteLine("Thank you {0}, now type your email:", userName);
+            string userEmail = Console.ReadLine();
+            var NewClan = new ClanCreation(userName, userEmail);
+            Console.WriteLine("What would you like to do?\n[1]View List of Players\n[2]Attack a player\n[3]View your stats");
         }
     }
-
-
 }
+
+
