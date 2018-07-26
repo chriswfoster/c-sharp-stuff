adbaba14b5cb409a85811288ccf72ab8b23b4388
chriswfoster@gmail.com <spinsci@SpinScis-MacBook-Pro-3.local>
2018-07-26 11:28:30 -0500

    Bank account

---
+++ People.cs
@@ -0,0 +1,51 @@
+// using System;
+
+// namespace c_sharp
+// {
+// // class MainClass {
+// //   public static void Main (string[] args) {
+// // // int i = 1;
+// // // int j = 2;
+// // // float k = 6%4; /*k = 3!*/
+// //   //   Console.WriteLine (1 > 2);
+// //   // }
+// //   }
+// // }
+// public class Person
+// {
+//   public Person()
+//   {
+//     Name = "unknown";
+//   }
+//   public Person(string name)
+//   {
+//     Name = name;
+//   }
+//   public String Name {get;}
+//   public override string ToString()
+//   {
+//     return Name;
+//   }
+// }
+
+// class TestPerson
+// {
+//   static void Alternative()
+//   {
+//     var person1 = new Person();
+//     Console.WriteLine(person1.Name);
+
+//     var person2 = new Person("Sarah Jones");
+//     Console.WriteLine(person2.Name);
+
+//     Console.WriteLine(person2);
+
+// Console.WriteLine("Press any key to exit");
+// Console.ReadKey();
+//   }
+// }
+// }
+
+// // Customer object1 = new Customer();
+
+// // Console.WritedoLine(object1.numbaz);
